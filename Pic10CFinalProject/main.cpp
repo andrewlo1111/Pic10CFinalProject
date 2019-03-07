11de1617373f2154a7071d1df032d99ce3ec4276
andrewlo1111 <andrewlo2528@gmail.com>
2019-03-06 21:18:10 -0800

    Changes widget when end turn

--- Pic10CFinalProject/main.cpp
+++ Pic10CFinalProject/main.cpp
@@ -5,6 +5,7 @@
 #include <QVBoxLayout>
 #include <QLabel>
 #include <QPushButton>
+#include <QButtonGroup>
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +14,8 @@ int main(int argc, char *argv[])
     QStackedWidget *control_window = new QStackedWidget;
     QWidget *player_one_info = new QWidget;
     QWidget *player_two_info = new QWidget;
+    QButtonGroup *end_turn_buttons = new QButtonGroup;
+
 
     QVBoxLayout *player_one_lay = new QVBoxLayout;
     QLabel *p1_label = new QLabel("Player 1 turn");
@@ -30,6 +33,8 @@ int main(int argc, char *argv[])
     QLabel *p2_farm_label = new QLabel("Farm Count: 0");
     QPushButton *p2_end_turn_button = new QPushButton("End Turn");
 
+    end_turn_buttons->addButton(p1_end_turn_button,1);
+    end_turn_buttons->addButton(p2_end_turn_button,0);
 
     player_one_lay->addWidget(p1_label);
     player_one_lay->addWidget(p1_money_label);
@@ -48,13 +53,18 @@ int main(int argc, char *argv[])
     player_two_lay->addWidget(p2_farm_label);
     player_two_lay->addWidget(p2_end_turn_button);
 
-    player_one_info->setLayout(player_two_lay);
+    player_two_info->setLayout(player_two_lay);
 
-    QObject::connect(p1_end_turn_button,SIGNAL(clicked()),control_window,SLOT(setCurrentIndex(int)));
 
-    control_window->setWindowTitle("Player 1 Information");
+
+
+
+    control_window->setWindowTitle("Player Info");
     control_window->addWidget(player_one_info);
     control_window->addWidget(player_two_info);
+
+
+    QObject::connect(end_turn_buttons,SIGNAL(buttonClicked(int)),control_window,SLOT(setCurrentIndex(int)));
     control_window->setGeometry(200,200,200,200);
     control_window->show();
     w.show();
